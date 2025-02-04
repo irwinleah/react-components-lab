62f5a99eb3cf69c6401f098ce90b673d68f23c9f
Leah Irwin <irwin.leah@outlook.com>
2025-02-04 15:28:27 -0500

    lab

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,14 @@
+import './WeatherForecast.css'
+
+const WeatherForecast = ({ weather }) => {
+return (<div className="weather">
+        <h2>{weather.day}</h2>
+        <img src={weather.img} alt={weather.imgAlt} />
+        <p><span>conditions: {weather.conditions} </span>current weather conditions</p>
+        <p><span>time: {weather.time} </span>time of day</p>
+    </div>
+)
+};
+
+
+export default WeatherForecast;
